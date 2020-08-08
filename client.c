ef05d226727f3dd81500256d0974f8ba4044a329
sl <workshilei@163.com>
2020-08-08 10:44:54 +0800

    client8.8

--- client.c
+++ client.c
@@ -1,6 +1,111 @@
+/***************************************************
+ * Name:       client.c
+ * Data:       2020.8.7
+ * Description:主函数
+ * Author:     shilei
+ *
+ * ************************************************/
+
 #include "client.h"
+#ifndef __MY_STRUCT__
+#define __MY_STRUCT__
+
+typedef struct
+{
+	char name[20];
+	double salary;
+	char department[20];
+	double telephone;
+	char E_mail[30];
+	char address[100];
+	char history[50];
+}EmpMsg;
+
+typedef struct
+{
+	char username[20];
+	char userpsw[20];
+	EmpMsg info;
+}data_t;
+
+typedef struct
+{
+	int protocol;//命令位
+	data_t data;
+}DATA;
+
+
+struct sockaddr_in sockaddr;
+/*
+ *1.创建套接字
+ *2.初始化客户端与服务器连接函数
+ *
+ */
+int net(void)
+{
+	int sockfd,ret;
+	//创建网络套接字
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0)){
+		perror("socket is error\n");
+		return -1;
+	}
+	//连接服务器
+	ret = connect(sockfd, (struct sockaddr *)&sockaddr,sizeof(sockaddr));
+	if(ret < 0){
+		perror("connect is error\n");
+		return -1;
+	}
+	return sockfd;
+}
 int main(int argc, const char *argv[])
 {
+	int sockfd,ret;
+	DATA *command;//初始化信息结构体
+	command = (DATA *)malloc(sizeof(DATA));
+	if(command == NULL){
+		perror("malloc error\n");
+		return -1;
+	}
+	//判断命令行传参是否错误
+	if(argc != 3){
+		printf("%s server ip\n",argv[0]);
+		return -1;
+	}
+	//清零结构体
+	memset(&sockaddr,0,sizeof(sockaddr));
+	sockaddr.sin_family = AF_INET;
+	sockaddr.sin_addr.s_addr = inet_addr(argv[1]);
+	sockaddr.sin_port = htons(atoi(argv[2]));
 	
+	sockfd = net();
+
+	while(1)
+	{
+		printf("员工管理系统\n");
+		memset(command,0,sizeof(DATA));
+		name_func(command->data.username);//判断输入的用户名是否合法
+		psw_func(command->data.userpsw);//判断输入的密码是否合法
+		command->protocol = 1;//请求登录
+		
+		//发送数据
+		ret = send(sockfd,command,sizeof(DATA),0);
+		if(ret < 0 ){
+			perror("send error\n");
+			return -1;
+		}
+		memset(command,0,sizeof(DATA))；
+		ret = recv(sockfd,0,sizeof(DATA),0);
+		if(ret < 0){
+			perror("recv error\n");
+			return -1;
+		}
+		if(command->protocol == "登录成功"){
+		
+		}else{
+			printf("登录失败！\n");
+			return -1;
+		}
+	}
+
 	return 0;
 }
---
+++ name_pws_func.c
@@ -0,0 +1,69 @@
+/***************************************************               
+ * Name:       name_pws_func.c
+ * Data:       2020.8.7
+ * Description:用户名和密码函数
+ * Author:     shilei
+ *
+ *************************************************/
+#include "client.h"
+/*
+ * Name:        string_func
+ * Description: 判断输入的字符串是否在0-9、a-z、A-Z之间,成功返回0，失败返回1
+ * Inputs:      str
+ *
+ */
+int string_func(char *str)
+{
+	int i;
+	for(i=0;i<strlen(str);i++){
+		if(!((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void name_func(char *username)
+{
+	char name[100];
+	while(1)
+	{
+		memset(namem,0,sizeof(name));
+		printf("请输入用户名\n");
+		//从stdin中获取用户名
+		fgets(name,sizeof(name),stdin);
+		//在数组结尾添加\0
+		name[strlen(name)-1] = '\0';
+		if(strlen(name) == 0){
+			printf("请输入用户名\n");
+			continue;
+		}
+
+		if(string_func(name) == 1){
+			printf("名字非法，请重新输入！\n");
+			continue;
+		}else
+			break;
+	}
+	strcat(username,name);
+	return;
+}
+
+void psw_func(char *userpsw)
+{
+	char psw[100];
+	while(1)
+	{
+		memset(psw,0,sizeof(psw));
+		printf("请输入密码：\n");
+		fgets(psw,sizeof(psw),stdin);
+		psw[strlen(psw)-1] = '\0';
+		if(string_func(psw) == 1){
+			printf("密码非法，请重新输入！\n");
+			continue;
+		}else
+			break;
+	}
+	strcat(userpsw,psw);
+	return;
+}
