33dfb995d7c37abf040352d1c14fb24333331833
sl <workshilei@163.com>
2020-08-08 18:04:21 +0800

    client8.81

--- client.c
+++ client.c
@@ -7,25 +7,35 @@
  * ************************************************/
 
 #include "client.h"
-#ifndef __MY_STRUCT__
-#define __MY_STRUCT__
 
+#define CLI_LOGIN 		1 	//客户端请求登录
+#define LOG_SUC 		2 	//登录成功
+#define LOG_QUIT 		3 	//登录退出
+#define LOG_SHW_INF 	4 	//查看个人信息
+#define LOG_CGE_PSW 	5 	//修改密码
+#define CGE_PSW_OK 		6 	//修改密码成功
+#define LOG_CGE_NAME 	7 	//修改名字
+#define CGE_NAME_OK 	8 	//修改名字成功
+#define LOG_CGE_YEAR 	9 	//修改年龄
+#define CGE_YEAR_OK  	10 	//修改年龄成功
 typedef struct
 {
 	char name[20];
+	char sex[10];
+	int year;
 	double salary;
 	char department[20];
 	double telephone;
 	char E_mail[30];
 	char address[100];
 	char history[50];
-}EmpMsg;
+}empinfo_t;
 
 typedef struct
 {
 	char username[20];
 	char userpsw[20];
-	EmpMsg info;
+	empinfo_t info;
 }data_t;
 
 typedef struct
@@ -85,12 +95,12 @@ int main(int argc, const char *argv[])
 		memset(command,0,sizeof(DATA));
 		name_func(command->data.username);//判断输入的用户名是否合法
 		psw_func(command->data.userpsw);//判断输入的密码是否合法
-		command->protocol = 1;//请求登录
+		command->protocol = CLI_LOGIN;//请求登录
 		
 		//发送数据
 		ret = send(sockfd,command,sizeof(DATA),0);
 		if(ret < 0 ){
-			perror("send error\n");
+			perror("client send error\n");
 			return -1;
 		}
 		memset(command,0,sizeof(DATA))；
@@ -99,13 +109,14 @@ int main(int argc, const char *argv[])
 			perror("recv error\n");
 			return -1;
 		}
-		if(command->protocol == "登录成功"){
-		
+		if(command->protocol == LOG_SUC){
+			login_func(command,sockfd);
 		}else{
 			printf("登录失败！\n");
 			return -1;
 		}
 	}
-
+	free(command);
+	close(sockfd);
 	return 0;
 }
